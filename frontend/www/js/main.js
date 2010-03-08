957f815cd1c7d70b44de450751b894fc2894de56
Manu Mora Gordillo <manuito@gmail.com>
2010-03-08 18:04:52 +0000

    Añadiendo iconos y precarga
    git-svn-id: file:///opt/svn/colegios/controlaula/trunk@346 c4774de7-f4ed-435d-9225-5986420fcf7b

--- frontend/www/js/main.js
+++ frontend/www/js/main.js
@@ -8,11 +8,14 @@
  */
 
 Ext.onReady(function() {
-	Ext.QuickTips.init();	
 	initScreens();
 
-     // tree.render('tree');
-    //  tree.bodyFocus.fi.setFrameEl(tree.el);
-    //  tree.getSelectionModel().select(tree.getRootNode());
-   //   tree.enter.defer(100, tree);
+    var hideMask = function () {
+        Ext.get('loading').remove();
+        Ext.fly('loading-mask').fadeOut({
+            remove:true,
+        });
+    }
+
+    hideMask.defer(250);
 });
