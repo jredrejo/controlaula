bd0ec023f5beea0649ba855001dc075129b06110
José L. Redrejo Rodriguez <jredrejo@debian.org>
2010-09-16 19:12:17 +0000

    Cambiada extensión de la página de inicio de .htm a .html
    git-svn-id: file:///opt/svn/colegios/controlaula/trunk@627 c4774de7-f4ed-435d-9225-5986420fcf7b

--- Backend/src/monitor.py
+++ Backend/src/monitor.py
@@ -169,7 +169,7 @@ if __name__ == '__main__':
         #print WEBPORT
         f = open(os.path.join(Configs.APP_DIR,'launcher.html') , 'wb')
         htmltext='<html><head>  <meta http-equiv="Refresh" content="0; url='
-        htmltext +='http://localhost:' + str(WEBPORT) + '/index.htm"'
+        htmltext +='http://localhost:' + str(WEBPORT) + '/index.html"'
         htmltext +='</head><body></body></html>'
         f.write(htmltext)
         f.close()  
