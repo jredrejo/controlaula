89ab3b65c88bdc0dcd3bda18db47ad22ffff95b2
Manu Mora Gordillo <manuito@gmail.com>
2010-10-05 07:56:16 +0000

    Añadiendo comprobaciones a setCookies
    git-svn-id: file:///opt/svn/colegios/controlaula/trunk@659 c4774de7-f4ed-435d-9225-5986420fcf7b

--- scripts/setCookies.py
+++ scripts/setCookies.py
@@ -1,6 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+##############################################################################
+# Project:     Controlaula
+# Module:      setCookies.py
+# Purpose:     Set cookies for user into Mozilla Firefox and Google Chrome
+# Language:    Python 2.5
+# Date:        5-Oct-2010.
+# Ver.:        5-Oct-2010.
+# Copyright:    2009-2010 - Manu Mora Gordillo       <manuito @nospam@ gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 
+# Important:    WHEN EDITING THIS FILE, USE TABS TO INDENT - NOT SPACES!
+##############################################################################
+
+
 from pysqlite2 import dbapi2 as sqlite
 import getpass # Availability: Unix, Windows.
-
+import os
 
 def getPathCookiesFirefox():
 	path = "/home/"+getpass.getuser()+"/.mozilla/firefox/"
@@ -14,27 +42,44 @@ def getPathCookiesFirefox():
 
 def setCookieFirefox ():
 
-	pathCookies = getPathCookiesFirefox()
+	path = getPathCookiesFirefox()
+	pathCookies = path+"cookies.sqlite"
 
-	connection = sqlite.connect(pathCookies)
-	#memoryConnection = sqlite.connect(':memory:')
-	cursor = connection.cursor()
+	if os.path.isdir(path):
+		if not os.path.isfile(pathCookies):
+			# create cookies.sqlite BD
 
-	cursor.execute("DELETE * FROM coockies WHERE name='controlaula.loginname='")
-	connection.commit()
+		connection = sqlite.connect(path+"cookies.sqlite")
+		#memoryConnection = sqlite.connect(':memory:')
+		cursor = connection.cursor()
 
-	cursor.execute("INSERT INTO coockies (id, name, value) VALUES (null,'controlaula.loginname','"+getpass.getuser()+"')");
-	connection.commit()
+		cursor.execute("DELETE * FROM coockies WHERE name='controlaula.loginname='")
+		connection.commit()
+
+		cursor.execute("INSERT INTO coockies (id, name, value) VALUES (null,'controlaula.loginname','"+getpass.getuser()+"')");
+		connection.commit()
 
 
 def setCookieChrome ():
+	path = "/home/"+getpass.getuser()+"/.config/google-chrome/Default/"
+	pathCookies = "/home/"+getpass.getuser()+"/.config/google-chrome/Default/Cookies"
 
-	connection = sqlite.connect("/home/"+getpass.getuser()+"/.config/google-chrome/Default/Cookies")
-	#memoryConnection = sqlite.connect(':memory:')
-	cursor = connection.cursor()
+	if os.path.isdir(path):
+		if not os.path.isfile(pathCookies):
+			# create Cookies BD
 
-	cursor.execute("DELETE * FROM coockies WHERE name='controlaula.loginname='")
-	connection.commit()
+		connection = sqlite.connect(pathCookies)
+		#memoryConnection = sqlite.connect(':memory:')
+		cursor = connection.cursor()
 
-	cursor.execute("INSERT INTO coockies (id, name, value) VALUES (null,'controlaula.loginname','"+getpass.getuser()+"')");
-	connection.commit()
+		cursor.execute("DELETE * FROM coockies WHERE name='controlaula.loginname='")
+		connection.commit()
+
+		cursor.execute("INSERT INTO coockies (id, name, value) VALUES (null,'controlaula.loginname','"+getpass.getuser()+"')");
+		connection.commit()
+
+
+if __name__ == '__main__':
+
+	setCookieFirefox ()
+	setCookieChrome ()
