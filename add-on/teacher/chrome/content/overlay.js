6db41f05fcb20fd66c1b7ddf3cedd04464246f3e
Manu Mora Gordillo <manuito@gmail.com>
2010-12-19 16:27:37 +0000

    Añadiendo el add-on para profesores
    git-svn-id: file:///opt/svn/colegios/controlaula/trunk@755 c4774de7-f4ed-435d-9225-5986420fcf7b

---
+++ add-on/teacher/chrome/content/overlay.js
@@ -0,0 +1,78 @@
+var ControlAula = {
+  onLoad: function() {
+    // initialization code
+    this.initialized = true;
+    this.strings = document.getElementById("ControlAula-strings");
+    
+    this.addToolbarButton();
+    
+  },
+
+
+  onToolbarButtonCommand: function(e) {
+    // just reuse the function above.  you can change this, obviously!
+    ControlAula.onMenuItemCommand(e);
+  },
+
+   
+   addToolbarButton : function() {
+       
+       
+ try {
+   var firefoxnav = document.getElementById("nav-bar");
+   var curSet = firefoxnav.currentSet;
+   if (curSet.indexOf("ControlAula-toolbar-button") == -1)
+   {
+     var set;
+     // Place the button before the urlbar
+     if (curSet.indexOf("urlbar-container") != -1)
+       set = curSet.replace(/urlbar-container/, "ControlAula-toolbar-button,ControlAula-toolbar-button2,urlbar-container");
+     else  // at the end
+       set = curSet + ",ControlAula-toolbar-button, ControlAula-toolbar-button2";
+     firefoxnav.setAttribute("currentset", set);
+     firefoxnav.currentSet = set;
+     document.persist("nav-bar", "currentset");
+
+     // If you don't do the following call, funny things happen
+     try {
+       BrowserToolboxCustomizeDone(true);
+     }
+     catch (e) { }
+   }
+
+ }
+ catch(e) { }
+ 
+        
+   }
+};
+
+window.addEventListener("load", ControlAula.onLoad, false);
+
+
+function sendLink() {
+
+	var doc = gBrowser.contentDocument,
+		href = doc.location.href;
+
+	// Is it the Google Maps page?
+	if (isMapsURL(href)){
+		// Then try to send the current view:
+		var link = doc.getElementById('link');
+		if (link && link.href)
+			href = link.href;
+	}
+
+	var focusedWindow = document.commandDispatcher.focusedWindow;
+	var selection = focusedWindow.getSelection().toString();
+
+	// send link with Ajax
+	xmlhttp = new XMLHttpRequest(); 
+	xmlhttp.open("POST", "http://localhost:8900/launchwebToAll",true);
+	xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	xmlhttp.send("url="+href+"&title="+doc.title);
+}
+
+function isMapsURL(url){
+	return url.match("http://maps\\.google\\.[a-z]{2,3}(\\.[a-z]{2})?[/?].*") ||	url.match("http://www\\.google\\.[a-z]{2,3}(\\.[a-z]{2})?/maps.*");
+}
