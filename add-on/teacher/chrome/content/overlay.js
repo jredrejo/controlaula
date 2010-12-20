84d84bf35984fa144e5233ed0aba3e9faf03ee1b
Manu Mora Gordillo <manuito@gmail.com>
2010-12-20 18:03:08 +0000

    Solucionado el problema de los dos iconos del addon
    git-svn-id: file:///opt/svn/colegios/controlaula/trunk@759 c4774de7-f4ed-435d-9225-5986420fcf7b

--- add-on/teacher/chrome/content/overlay.js
+++ add-on/teacher/chrome/content/overlay.js
@@ -21,24 +21,35 @@ var ControlAula = {
  try {
    var firefoxnav = document.getElementById("nav-bar");
    var curSet = firefoxnav.currentSet;
-   if (curSet.indexOf("ControlAula-toolbar-button") == -1)
-   {
+
+   if (curSet.indexOf("ControlAula-toolbar-button") == -1){
      var set;
      // Place the button before the urlbar
      if (curSet.indexOf("urlbar-container") != -1)
-       set = curSet.replace(/urlbar-container/, "ControlAula-toolbar-button,ControlAula-toolbar-button2,urlbar-container");
+       set = curSet.replace(/urlbar-container/, "ControlAula-toolbar-button,urlbar-container");
      else  // at the end
-       set = curSet + ",ControlAula-toolbar-button, ControlAula-toolbar-button2";
+       set = curSet + ",ControlAula-toolbar-button";
      firefoxnav.setAttribute("currentset", set);
      firefoxnav.currentSet = set;
      document.persist("nav-bar", "currentset");
+	}
+   if (curSet.indexOf("ControlAula-toolbar-button2") == -1){
+     var set;
+     // Place the button before the urlbar
+     if (curSet.indexOf("urlbar-container") != -1)
+       set = curSet.replace(/urlbar-container/, "ControlAula-toolbar-button2,urlbar-container");
+     else  // at the end
+       set = curSet + ",ControlAula-toolbar-button2";
+     firefoxnav.setAttribute("currentset", set);
+     firefoxnav.currentSet = set;
+     document.persist("nav-bar", "currentset");
+	}
 
-     // If you don't do the following call, funny things happen
-     try {
-       BrowserToolboxCustomizeDone(true);
-     }
-     catch (e) { }
+   // If you don't do the following call, funny things happen
+   try {
+     BrowserToolboxCustomizeDone(true);
    }
+   catch (e) { }
 
  }
  catch(e) { }
