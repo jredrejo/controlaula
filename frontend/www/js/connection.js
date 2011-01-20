80f5092d6fe01239b0e6b82a72d56a05cfa3a469
Manu Mora Gordillo <manuito@gmail.com>
2011-01-20 18:30:33 +0000

    Añadiendo cabeceras
    git-svn-id: file:///opt/svn/colegios/controlaula/trunk@769 c4774de7-f4ed-435d-9225-5986420fcf7b

--- frontend/www/js/connection.js
+++ frontend/www/js/connection.js
@@ -1,12 +1,24 @@
-/*
- * connection.js
- *
- * Copyright (c) 2009-2010 Manu Mora Gordillo <manuito @nospam@ gmail.com>
- * Licensed under the GPL (GPL-LICENSE.txt) license.
- *
- * 
- */
-
+/* #########################################################################
+# Project:     Controlaula
+# Module:     	connection.js
+# Purpose:     Connection with the backend
+# Language:    javascript
+# Copyright:   2009-2010 - Manuel Mora Gordillo <manuito @nospam@ gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 
+############################################################################## */
 var dataRefresh = $.JSON.encode({"args" : "refresh"});
 
 
