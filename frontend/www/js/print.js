7336592cb54f7de5e1186271d99f9c6474b4a79a
José L. Redrejo Rodriguez <jredrejo@debian.org>
2011-09-08 09:10:18 +0000

    modificaciones para funcionar con las versiones más recientes de firefox
    git-svn-id: file:///opt/svn/colegios/controlaula/trunk@834 c4774de7-f4ed-435d-9225-5986420fcf7b

--- frontend/www/js/print.js
+++ frontend/www/js/print.js
@@ -75,39 +75,39 @@ function printSliders(){
 
 function printClassroom(equipos){
 
-	var class = eval('(' + equipos + ')');
+	var myclass = eval('(' + equipos + ')');
 
-	if(class.classroom.pclist.length==0)
+	if(myclass.classroom.pclist.length==0)
 		return;
-//alert(class.classroom.structure.rows+" - "+class.classroom.structure.cols);
+//alert(myclass.classroom.structure.rows+" - "+myclass.classroom.structure.cols);
 
-	setColsRows(class.classroom.structure.cols, class.classroom.structure.rows);   
-	setComputers(class.classroom.structure.computers);
+	setColsRows(myclass.classroom.structure.cols, myclass.classroom.structure.rows);   
+	setComputers(myclass.classroom.structure.computers);
 
 	//$('#selectable .ui-state-default').remove();
 
-	for(var i=0;i<class.classroom.pclist.length;i++){
+	for(var i=0;i<myclass.classroom.pclist.length;i++){
 
-		var name = class.classroom.pclist[i].loginname;
-		var photo = class.classroom.pclist[i].photo;
-		var pcname = class.classroom.pclist[i].PCname;
+		var name = myclass.classroom.pclist[i].loginname;
+		var photo = myclass.classroom.pclist[i].photo;
+		var pcname = myclass.classroom.pclist[i].PCname;
 		var internet=mouse=message="img/pcNone.png";
 		
-		if(class.classroom.pclist[i].PCname=="none"){
+		if(myclass.classroom.pclist[i].PCname=="none"){
 			pcname = "&nbsp;";
 			name = "&nbsp;";
 			photo = "img/pcNone.png";			
-		}else if(class.classroom.pclist[i].ON=="0"){
+		}else if(myclass.classroom.pclist[i].ON=="0"){
 			name =  translations["Off"];
 			photo = "img/turnOff.png";
-		}else if(class.classroom.pclist[i].loginname=="" && class.classroom.pclist[i].ON=="1"){
+		}else if(myclass.classroom.pclist[i].loginname=="" && myclass.classroom.pclist[i].ON=="1"){
 			name = "Login";
 			photo = "img/turnOn.png";
 		}
 
-		if(class.classroom.pclist[i].internetEnabled=="1") internet="img/icon_web.png";
-		if(class.classroom.pclist[i].mouseEnabled=="1") mouse="img/icon_mouse.png";
-		if(class.classroom.pclist[i].messagesEnabled=="1") message="img/icon_messages.png";
+		if(myclass.classroom.pclist[i].internetEnabled=="1") internet="img/icon_web.png";
+		if(myclass.classroom.pclist[i].mouseEnabled=="1") mouse="img/icon_mouse.png";
+		if(myclass.classroom.pclist[i].messagesEnabled=="1") message="img/icon_messages.png";
 
 		$('#selectable').append('<li class="ui-state-default" id="selectable-'+i+'"><div id="pcName">'+pcname+'</div><div style="float:right;"><img src="'+internet+'" style="width:20px; height:20px;"><br><img src="'+mouse+'" style="padding-top:3px; width:20px; height:20px;"><br><img src="'+message+'" style="padding-top:0px; width:20px; height:20px;"></div><img class="thumb-image" src="'+photo+'"/><div id="userName">'+name+'</div></li>');
 		//$('#selectable').append('');
@@ -115,39 +115,39 @@ function printClassroom(equipos){
 
 		$('#sortable').append('<li class="ui-state-default" id="sortable-'+i+'"><div id="pcName">'+pcname+'</div><img class="thumb-image" src="'+photo+'"/><div id="userName">'+name+'</div></li>');
 	}	
-	lastPClist = class.classroom.pclist;
+	lastPClist = myclass.classroom.pclist;
 }
 
 function refreshClassroom(equipos){
-	var class = eval('(' + equipos + ')');
+	var myclass = eval('(' + equipos + ')');
 
-	if(class.classroom.pclist.length==0)
+	if(myclass.classroom.pclist.length==0)
 		return;
 
 	//$('#selectable .ui-state-default').remove();
 
-	for(var i=0;i<class.classroom.pclist.length;i++){
+	for(var i=0;i<myclass.classroom.pclist.length;i++){
 
-		var name = class.classroom.pclist[i].loginname;
-		var photo = class.classroom.pclist[i].photo;
-		var pcname = class.classroom.pclist[i].PCname;
+		var name = myclass.classroom.pclist[i].loginname;
+		var photo = myclass.classroom.pclist[i].photo;
+		var pcname = myclass.classroom.pclist[i].PCname;
 		var internet=mouse=message="img/pcNone.png";
 		
-		if(class.classroom.pclist[i].PCname=="none"){
+		if(myclass.classroom.pclist[i].PCname=="none"){
 			pcname = "&nbsp;";
 			name = "&nbsp;";
 			photo = "img/pcNone.png";			
-		}else if(class.classroom.pclist[i].ON=="0"){
+		}else if(myclass.classroom.pclist[i].ON=="0"){
 			name = translations["Off"];
 			photo = "img/turnOff.png";
-		}else if(class.classroom.pclist[i].loginname=="" && class.classroom.pclist[i].ON=="1"){
+		}else if(myclass.classroom.pclist[i].loginname=="" && myclass.classroom.pclist[i].ON=="1"){
 			name = "Login";
 			photo = "img/turnOn.png";
 		}
 
-		if(class.classroom.pclist[i].internetEnabled=="1") internet="img/icon_web.png";
-		if(class.classroom.pclist[i].mouseEnabled=="1") mouse="img/icon_mouse.png";
-		if(class.classroom.pclist[i].messagesEnabled=="1") message="img/icon_messages.png";
+		if(myclass.classroom.pclist[i].internetEnabled=="1") internet="img/icon_web.png";
+		if(myclass.classroom.pclist[i].mouseEnabled=="1") mouse="img/icon_mouse.png";
+		if(myclass.classroom.pclist[i].messagesEnabled=="1") message="img/icon_messages.png";
 
 		var refresh = '<img src="'+photo+'"/>'+name;
 
@@ -163,7 +163,7 @@ function refreshClassroom(equipos){
 		$('#sortable').append('<li class="ui-state-default" id="sortable-'+i+'"><div id="pcName">'+pcname+'</div><img class="thumb-image" src="'+photo+'"/><div id="userName">'+name+'</div></li>');
 
 	}
-	lastPClist = class.classroom.pclist;
+	lastPClist = myclass.classroom.pclist;
 }
 
 var computerSelected="";
